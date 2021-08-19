08794b435d013269a1f291edfbf09357eb05a09f
KF-BERG <79504951+KF-BERG@users.noreply.github.com>
2021-08-19 15:00:35 +0200

    remove random image  from companyList

--- FirmyMichalowiceSPA/src/app/companyCard/CompanyCard.component.ts
+++ FirmyMichalowiceSPA/src/app/companyCard/CompanyCard.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Company } from '../_models/company';
+import { UploadPhotoService } from '../_services/uploadPhoto.service';
 
 @Component({
   selector: 'app-CompanyCard',
@@ -8,12 +10,32 @@ import { Company } from '../_models/company';
 })
 export class CompanyCardComponent implements OnInit {
   @Input() company: Company;
+  imageToShow: any;
+  noImageFound: boolean;
 
-  constructor(){ }
+  constructor( private uploadPhotoService: UploadPhotoService,
+               private sanitizer: DomSanitizer){
+   }
 
-  ngOnInit() : void{
-   
-  }
-
-
+// tslint:disable-next-line:typedef
+ngOnInit() {
+  this.getImage(this.company.id);
 }
+
+  // tslint:disable-next-line:typedef
+  getImage(userId: number) {
+    const mediaType = 'application/image';
+    this.uploadPhotoService.getImage(userId)
+      .subscribe(data => {
+        const blob = new Blob([data], { type: mediaType });
+        const unsafeImg = URL.createObjectURL(blob);
+        this.imageToShow = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+    }, error => {
+        console.log(error);
+        this.noImageFound = true;
+    });
+
+  }
+}
+
+
