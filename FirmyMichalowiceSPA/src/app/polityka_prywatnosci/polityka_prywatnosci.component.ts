3802be13a4e108e8f6d5141f7639a65e1520e85e
Karol <karol.franczak@outlook.com>
2021-10-28 12:02:54 +0200

    remove fonts

--- FirmyMichalowiceSPA/src/app/polityka_prywatnosci/polityka_prywatnosci.component.ts
+++ FirmyMichalowiceSPA/src/app/polityka_prywatnosci/polityka_prywatnosci.component.ts
@@ -4,7 +4,7 @@ import {Location} from '@angular/common';
 @Component({
   selector: 'app-polityka_prywatnosci',
   templateUrl: './polityka_prywatnosci.component.html',
-  styleUrls: ['./polityka_prywatnosci.component.scss']
+  styleUrls: ['./polityka_prywatnosci.component.css']
 })
 export class Polityka_prywatnosciComponent implements OnInit {
 
