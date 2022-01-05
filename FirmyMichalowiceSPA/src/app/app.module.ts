f5cc0953800d32de446899e5de9291e8cdc31e5c
Karol <karol.franczak@outlook.com>
2022-01-05 16:48:25 +0100

    fix bootstrap

--- FirmyMichalowiceSPA/src/app/app.module.ts
+++ FirmyMichalowiceSPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes  } from '@angular/router';
+import { RouterModule  } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
